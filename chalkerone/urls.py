213419a14b2512aa90bf36be0e50b56b7f0ec934
jayarghargh <jr@williamreid.com.au>
2020-02-01 18:06:46 +1030

    Facebook integration :)

--- chalkerone/urls.py
+++ chalkerone/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('', include('pages.urls')),
+    url(r'^accounts/',include('allauth.urls')),
     # path('accounts/', include('allauth.urls')),
 ]
