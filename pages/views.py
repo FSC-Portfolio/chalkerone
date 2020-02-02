d370e807f5408f584f3ee0c872da5842bb647b97
jayarghargh <jr@williamreid.com.au>
2020-02-02 16:10:45 +1030

    Working the login and overall look

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,9 @@ from django.views.generic import TemplateView
 
 
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+
 class PrivacyPageView(TemplateView):
     template_name = 'pages/privacy.html'
