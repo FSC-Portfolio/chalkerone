9bc7ff048f3c56b98fd99bbbf54fb266b8a5175c
jayarghargh <jr@williamreid.com.au>
2020-02-04 12:59:56 +1030

    Added features and terms of service pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 # from .views import HomePageView, HelpPageView, NotFound, ViewMyProfile
-from .views import PrivacyPageView, HomePageView
+from .views import PrivacyPageView, HomePageView, FeaturesPageView, TermsPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('privacy', PrivacyPageView.as_view(), name='privacy'),
+    path('features', FeaturesPageView.as_view(), name='features'),
+    path('terms', TermsPageView.as_view(), name='terms_of_service'),
     # path('help', HelpPageView.as_view(), name='help'),
     # path('tournament', NotFound.as_view(), name='404'),
     # path('profile', ViewMyProfile.as_view(), name='my-profile'),
