1eac940af0e27bdfa3968da12183a38086dc954a
jayarghargh <jr@williamreid.com.au>
2020-02-04 21:12:56 +1030

    trying to fill in the metadata fro club creation.

--- club/views.py
+++ club/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView, CreateView
 from .models import DartClub
 from django.urls import reverse
+from django.shortcuts import get_object_or_404
 
 
 class ViewAllClubs(ListView):
@@ -15,8 +16,16 @@ class ClubDetailView(DetailView):
 
 class CreateClub(CreateView):
     model = DartClub
-    template_name = 'club/create-club.html'
+
     fields = ['club_name', 'date_established', 'club_status', 'club_member_fees', 'club_meeting_frequency', 'club_banner_image']
+    template_name = 'club/create-club.html'
+
+    # def get_initial(self):
+    #     state = get_object_or_404(User, id=User.objects.latest('id').id)
+    #     return {
+    #         'state': state,
+    #     }
+
 
     def get_success_url(self):
         return reverse('view_single_club', kwargs={'pk': self.object.id})
