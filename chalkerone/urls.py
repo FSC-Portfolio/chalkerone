cf82127d4676ccff29e10ae69ca85fe3137ef4c0
jayarghargh <jr@williamreid.com.au>
2020-02-05 17:24:39 +1030

    Club creation and main page mostly done (exception of the few bugs on githib)

--- chalkerone/urls.py
+++ chalkerone/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,5 @@ urlpatterns = [
     path('club/', include('club.urls')),
     url(r'^accounts/',include('allauth.urls')),
     # path('accounts/', include('allauth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
