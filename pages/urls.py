ef9b80e917ae76a5c560abe5ff8e800e5f9a4ee5
jayarghargh <jr@williamreid.com.au>
2020-02-06 22:22:02 +1030

    Added live game concept page, and tutorials.html #23 and #20

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 # from .views import HomePageView, HelpPageView, NotFound, ViewMyProfile
-from .views import PrivacyPageView, HomePageView, FeaturesPageView, TermsPageView
+from .views import PrivacyPageView, HomePageView, FeaturesPageView, TermsPageView, TutorialsPageView, LiveGamePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('privacy', PrivacyPageView.as_view(), name='privacy'),
     path('features', FeaturesPageView.as_view(), name='features'),
     path('terms', TermsPageView.as_view(), name='terms_of_service'),
+    path('tutorials', TutorialsPageView.as_view(), name='tutorials'),
+    path('games/fn2187', LiveGamePageView.as_view(), name='live_game'),
     # path('help', HelpPageView.as_view(), name='help'),
     # path('tournament', NotFound.as_view(), name='404'),
     # path('profile', ViewMyProfile.as_view(), name='my-profile'),
